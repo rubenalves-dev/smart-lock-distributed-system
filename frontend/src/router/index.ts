35473a6f2dd18c6c2d982e675478b3e0157809b6
RodrigoVenturaepsm <183516841+Rodri20-dev@users.noreply.github.com>
2026-05-30 18:54:39 +0100

    Continuação da limpeza

--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -8,10 +8,10 @@ const router = createRouter({
   routes: [
     {
       path: '/',
-      name: 'Ecommerce',
-      component: () => import('../views/Ecommerce.vue'),
+      name: 'Dashboard',
+      component: () => import('../views/Dashboard.vue'),
       meta: {
-        title: 'eCommerce Dashboard',
+        title: 'Dashboard',
         fullWidth: true,
       },
     },
